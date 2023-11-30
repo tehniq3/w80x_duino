f4ac5467e87c41ea3a4c5ff4d6d036158db402d3
Nicu FLORICA (niq_ro) <nicu.florica@gmail.com>
2023-11-30 20:22:16 +0200

    Add files via upload

---
+++ w801_leds1.ino
@@ -0,0 +1,35 @@
+/* led effect: https://www.instructables.com/Audi-LED-Dynamic-Blinker-Arduino/
+* original ideea for w801 board by Nicu FLORICA (niq_ro)
+* internal leds at 11,16,17,18,26,25,5
+ */
+
+int led[7] = {PB11, PB16, PB17, PB18, PB26, PB25, PB5};
+
+int pauza = 100;
+
+
+ 
+void setup()
+
+{
+for (int i = 0; i < 7; i++)
+{
+ pinMode(led[i],OUTPUT); 
+ digitalWrite(led[i],1);
+}
+}
+void loop()
+{
+for (int i = 0; i < 7; i++)
+{
+ digitalWrite(led[i],0);
+ delay(pauza);
+}
+for (int i = 0; i < 7; i++)
+{
+ digitalWrite(led[i],1);
+ delay(pauza);
+}
+
+
+}  // end main loop
